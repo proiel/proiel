2dab1d536fdc97ed4deebfae190f02da0d78dc1f
Marius L. Jøhndal <mariuslj@ifi.uio.no>
2023-05-19 15:12:21 +0200

    Bump version number to 1.3.2.

--- lib/proiel/version.rb
+++ lib/proiel/version.rb
@@ -1,8 +1,8 @@
 #--
-# Copyright (c) 2015-2018 Marius L. Jøhndal
+# Copyright (c) 2015-2023 Marius L. Jøhndal
 #
 # See LICENSE in the top-level source directory for licensing terms.
 #++
 module PROIEL
-  VERSION = '1.3.1'.freeze
+  VERSION = '1.3.2'.freeze
 end
